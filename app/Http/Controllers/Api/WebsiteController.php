4d6f93d852b14b39ddb8767223db352474e5d7ba
Paulo Castellano <paulofcastellano@gmail.com>
2024-10-21 15:08:09 -0300

    working

--- app/Http/Controllers/Api/WebsiteController.php
+++ app/Http/Controllers/Api/WebsiteController.php
@@ -8,10 +8,6 @@ class WebsiteController extends Controller
 {
     public function favicon(Request $request)
     {
-        $request->validate([
-            'url' => ['required', 'max:255', 'url'],
-        ]);
-
         try {
             $favicon = file_get_contents("https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url={$request->url}&size=128");
         } catch (\Throwable $th) {
